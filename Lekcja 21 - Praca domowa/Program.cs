d335eed5ac949c6f02093043b1463a33005f08b6
PcFormat2023.02.Jakub <jakubgolebiewski14@gmail.com>
2023-04-30 15:13:06 +0200

    Add project files.

---
+++ Lekcja 21 - Praca domowa/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lekcja_21___Praca_domowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+
+                Console.WriteLine("Część, odpowiedz na kilka pytań abym mógł się odpowiedno z Tobą przyiwtać");
+                Console.WriteLine("Jak masz na imię");
+
+                //imię
+                string name = Console.ReadLine();
+
+                //powiększanie pierwszej litery imiena
+                name = char.ToUpper(name[0]) + name.Substring(1);
+
+                int yearOfBirth = GetYear();
+                int monthOfBith = GetMonth();
+                int dayOfBith = GetDay(yearOfBirth, monthOfBith);
+
+                // miejsce u rodzenia
+                Console.WriteLine("Powiedz gdzie sie urodziłeś");
+                string birthPlace = Console.ReadLine();
+
+                //powiększanie pierwszej litery miejscowości
+                birthPlace = char.ToUpper(birthPlace[0]) + birthPlace.Substring(1);
+
+                int age = DateTime.Now.Year - yearOfBirth;
+
+                if (yearOfBirth> DateTime.Now.DayOfYear)
+                    age--;
+
+                Console.WriteLine($"\n\nCześć {name} urodziłeś się w {birthPlace} i masz {age} lat!");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Nieprawidłowe dane");
+                throw ex;
+
+            }
+            finally
+            {
+                Console.WriteLine("Kliknij Enter aby zakończyć program");
+                Console.ReadLine();
+            }
+
+        }
+
+        private static int GetDay(int year, int month)
+        {
+            while (true)
+            {
+                Console.WriteLine("Podaj swój dzień urodzenia");
+                if (!int.TryParse(Console.ReadLine(), out int day)
+                    || day > DateTime.DaysInMonth(year, month)
+                    || day < 1)
+                {
+                    Console.WriteLine("Podano nieprawidłowy dzień, spróbuj ponownie\n");
+                    continue;
+
+                }
+
+                return day;
+            }
+        }
+
+        private static int GetMonth()
+        {
+            Console.WriteLine("Podaj swój miesiąc urodzenia");
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int month) 
+                    || month > 12 
+                    || month < 1) 
+                {
+                    Console.WriteLine("Podano nieprawidłowy miesiąc, spróbuj ponownie\n");
+                    continue; 
+                }
+
+                return month;
+            }
+        }
+
+        private static int GetYear()
+        {
+            while (true)
+            {
+                Console.WriteLine("Podaj swój rok urodzenia");
+                if (!int.TryParse(Console.ReadLine(), out int year) || year > DateTime.Now.Year)
+                {
+                    Console.WriteLine("Podano nieprawidłowy rok, spróbuj ponownie\n");
+                    continue;
+                }
+
+                return year;
+            }
+        }
+
+
+    }
+}
---
+++ Lekcja 21 - Praca domowa/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lekcja 21 - Praca domowa")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lekcja 21 - Praca domowa")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6dbbf635-7b3e-4846-9841-c8f9075e422b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
